2203751a8eed9146496690c4bf99dad9b09e91f6
justinpombrio <zallambo@gmail.com>
2015-06-24 15:37:29 -0700

    .

--- stlc.v
+++ stlc.v
@@ -23,14 +23,21 @@ Inductive Term : Set :=
 
 Definition is_value (t : Term) : Prop :=
 match t with
-  | true => True
-  | false => True
-  | ifte _ _ _ => False
-  | var _ => False
-  | lamb _ _ => True
-  | app _ _ => False
+  | true | false | lamb _ _      => True
+  | ifte _ _ _ | var _ | app _ _ => False
 end.
 
+Lemma value_case:
+  forall t, is_value t -> t = true \/
+                          t = false \/ 
+                          exists x b, t = lamb x b.
+Proof.
+  destruct t; try contradiction; intros.
+  - left; reflexivity.
+  - right; left; reflexivity.
+  - right; right. econstructor; econstructor. reflexivity.
+Qed.
+  
 
 
 Inductive Ctx : Set := (*big gamma*)
@@ -135,39 +142,11 @@ Inductive Step : Term -> Term -> Prop :=
   | step_app : forall x a b, Step (app (lamb x b) a) (subs1 x a b).
 
 
-Inductive Halts : Term  -> Prop :=
+Inductive Halts : Term -> Prop :=
   | h_true :  Halts true
   | h_false : Halts false
   | h_lamb :  forall x b, Halts (lamb x b)
   | h_step :  forall a b, Step a b -> Halts b -> Halts a.
-
-Lemma subs1_shadow:
-  forall c x A B G C,
-    #[cons (cons G x A) x B |- c @ C]
-    -> #[cons G x B |- c @ C].
-Proof.
-  intros c. induction c; intros; inversion H; subst.
-  - constructor.
-  - constructor.
-  - apply IHc1 in H3. apply IHc2 in H6. apply IHc3 in H7.
-    constructor; assumption.
-  - inversion H1.
-    destruct (string_eq x s) eqn:eq.
-    { inversion H2; subst.
-      apply d_var. simpl. rewrite eq. reflexivity. }
-    { apply d_var. simpl. rewrite eq. assumption. }
-  - apply d_lamb.
-    
-    
-
-
-
-
-
-  intros. induction c; inversion H; subst.
-  - constructor.
-  - constructor.
-  - intuition. apply IHc1 in H3.
 
 Lemma subs1_lemma:
   forall x a A,
@@ -175,34 +154,11 @@ Lemma subs1_lemma:
     -> forall b B, #[cons empty x A |- b @ B]
                    -> #[empty |- subs1 x a b @ B].
 Proof.
-(*  intros x a A d b. induction b.
-  admit. admit. admit. admit. intros. *)
-  intros x a A d b. induction b; intros; simpl; inversion H; subst.
-  - apply d_true.
-  - apply d_false.
-  - apply IHb1 in H3.
-    apply IHb2 in H6.
-    apply IHb3 in H7.
-    apply d_if; assumption.
-  - inversion H1. destruct (string_eq x s); congruence.
-  - simpl.
-    destruct (string_eq x s) eqn:eq.
-    constructor.
-    
-    
-    admit.
-  - simpl.
-    
-    
-    
 Admitted.
-
-
 
 Theorem type_preservation:
   forall a a', Step a a' ->
-               forall A, #[empty |- a @ A]
-                         -> #[empty |- a' @ A].
+               forall A, #[empty |- a @ A] -> #[empty |- a' @ A].
 Proof.
   intros a a' step.
   induction step.
@@ -219,41 +175,67 @@ Proof.
     eapply subs1_lemma; eassumption.
 Qed.
 
-(***Old big step version ***)
-  
-Inductive Step : Term -> Term -> Prop :=
-  | step_true  : forall a b, Step (ifte true a b) a
-  | step_false : forall a b, Step (ifte false a b) b
-  | step_app   : forall v a b, Step (app (lamb v b) a) (subs1 v a b).
+Ltac break_value H :=
+  (apply value_case in H; destruct H as [ a | [ b | c ] ]; intros; subst).
 
-Inductive Halts : Term -> Tipe -> Prop :=
-  | h_true :  Halts true t_bool
-  | h_false : Halts false
-  | h_lamb :  forall x b, Halts (lamb x b)
-  | h_step :  forall a b, Step a b -> Halts b -> Halts a.
+Theorem type_progress:
+  forall a A,
+    #[empty |- a @ A] -> is_value a \/ exists a', Step a a'.
+Proof.
+  intros. dependent induction H.
+  - left. apply I.
+  - left. apply I.
+  - right.
+    specialize IHDeriv1 with cond t_bool.
+    assert (is_value cond \/ exists a': Term, Step cond a').
+    apply IHDeriv1. reflexivity.
+    destruct H2.
+    { break_value H2.
+      { exists consq. apply step_if_true. }
+      { exists alt.   apply step_if_false. }
+      { 
+      
 
+
+      elim i; intros; subst.
+      { apply step_if_true. }
+      { simpl.
+      { 
+  - right. econstructor.
+    assert (is_value cond \/ exists a': Term, Step cond a').
+    { eapply IHDeriv1. reflexivity. }
+    inversion H2.
+
+    
+    
+
+
+
+(*
 Lemma halt_if: forall a b c,
                  Halts a -> Halts b -> Halts c -> Halts (ifte a b c).
 Proof.
   intros.
   induction H.
-  - eapply h_step. apply step_true. assumption.
-  - eapply h_step. apply step_false. assumption.
-  -.
-
+  - eapply h_step. apply step_if_true. assumption.
+  - eapply h_step. apply step_if_false. assumption.
+  - 
+(*  - simpl. eapply h_step. admit. eassumption. *)
+  - simpl.
+*)
 
 Fixpoint SN (a: Term) (A: Tipe) : Prop :=
   match A with
     | t_bool  =>
       #[empty |- a @ t_bool]
-      /\ Halts a A
+      /\ Halts a
     | A :-> B =>
         #[empty |- a @ A :-> B]
-        /\ Halts a A
+        /\ Halts a
         /\ (forall b, SN b A -> SN (app a b) B)
   end.
 
-Lemma sn__halt: forall a A, SN a A -> Halt a.
+Lemma sn__halt: forall a A, SN a A -> Halts a.
 Proof.
   intros.
   destruct A; unfold SN in H.
@@ -280,15 +262,15 @@ Lemma sn_ind: forall g G a A,
 Proof.
   intros.
   dependent induction H; simpl.
-  { split. apply d_true. apply halt_true. }
-  { split. apply d_false. apply halt_false. }
+  { split. apply d_true. apply h_true. }
+  { split. apply d_false. apply h_false. }
   { intuition. destruct A; simpl; split.
     { apply sn__type in H3. apply sn__type in H4.
       apply d_if; try assumption.
       apply sn__type.
       eapply IHDeriv1. reflexivity. assumption. }
     { apply sn__halt in H3. apply sn__halt in H4.
-      
+      apply 
 
 
 
